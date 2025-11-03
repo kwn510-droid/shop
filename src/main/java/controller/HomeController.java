2fdec7359c123c1fc14d5e1daf239cce4420848c
kwn510 <kwn510@SoonPyo>
2025-11-03 10:30:51 +0900

    커민 연습2 - hone.jsp 포워딩 구현

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -7,26 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class HomeController
- */
-@WebServlet("/HomeController")
+
+@WebServlet("/Home")
 public class HomeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/view.jsp");
 	}
-
 }
